0454ce1a29ac0c760033e8b44e9a61ceb556e230
Tim Lucca <tim.lucca88@gmail.com>
2020-06-07 20:03:58 -0600

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -165,7 +165,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_gol_bg.wasm":"5a6dcbf288d7d58d8a48"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_gol_bg.wasm":"1c2e73a6c56d7f2b5de5"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
